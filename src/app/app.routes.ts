b42bea14c5883c267e49f06d89df0ffb309b1a41
konatebeh20 <konatebeh20@gmail.com>
2025-01-30 18:06:21 +0000

    home/service/all-product/contact-(page)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,16 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './main/about/about.component';
+import { ContactUsComponent } from './main/contact-us/contact-us.component';
+import { ProductsComponent } from './main/products/products.component';
+import { ServicesComponent } from './main/services/services.component';
+import { TeamComponent } from './main/team/team.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/main', pathMatch: 'full' },
     { path: 'main', loadChildren: () => import('./main/main.module').then(m => m.MainModule) },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact-us', component: ContactUsComponent },
+    { path: 'products', component: ProductsComponent },
+    { path: 'services', component: ServicesComponent }, // Ajout de la route service
+    { path: 'team', component: TeamComponent }
 ];
--- src/app/main/products/product-details/product-details.component.ts
+++ src/app/main/products/product-details/product-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-details',
@@ -8,6 +9,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit{
 
+  product: any = null;
 
   all_products = [
     { product_id: 't-cashdash-001', product_label: 'Cashdash', product_link: './assets/img/logo-tornixtech/cashdash-logo.jpeg' },
@@ -17,8 +19,12 @@ export class ProductDetailsComponent implements OnInit{
     { product_id: 't-flowhub-005', product_label: 'Flowhub', product_link: './assets/img/logo-tornixtech/flowhub-logo.jpeg' },
     { product_id: 't-feedsy-006', product_label: 'Feedsy', product_link: './assets/img/logo-tornixtech/feedsy-logo.png' }
   ];
-  constructor(){}
+  constructor(private route: ActivatedRoute){}
   ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      const productId = params.get('id');
+      this.product = this.all_products.find(p => p.product_id === productId);
+    });
   }
 
 }
