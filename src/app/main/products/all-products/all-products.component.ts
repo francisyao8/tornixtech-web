0cf2893499ae996ea62133f1f2338df09d380f03
francis yao <francis.amani@tornixtech.com>
2025-01-30 19:05:51 +0000

    update products module

--- src/app/main/products/all-products/all-products.component.ts
+++ src/app/main/products/all-products/all-products.component.ts
@@ -13,7 +13,7 @@ export class AllProductsComponent implements OnInit{
 
   all_products = [
     { product_id: 't-cashdash-001', product_label: 'Cashdash', product_link: './assets/img/logo-tornixtech/cashdash-logo.jpeg' },
-    { product_id: 't-keletik-002', product_label: 'Keletik', product_link: './assets/img/logo-tornixtech/keletick-logo.png' },
+    { product_id: 't-keletcik-002', product_label: 'Keletcik', product_link: './assets/img/logo-tornixtech/keletick-logo.png' },
     { product_id: 't-goparadize-003', product_label: 'Goparadize', product_link: './assets/img/logo-tornixtech/goparadize-logo.png' },
     { product_id: 't-flotys-004', product_label: 'Flotys', product_link: './assets/img/logo-tornixtech/flotys-logo.jpeg' },
     { product_id: 't-flowhub-005', product_label: 'Flowhub', product_link: './assets/img/logo-tornixtech/flowhub-logo.jpeg' },
