5795a8f7c33018a1ee057a9be2a1e6f305babd91
konatebeh20 <konatebeh20@gmail.com>
2025-03-18 13:11:32 +0000

    services module

--- frontend/src/app/main/main-routing.module.ts
+++ frontend/src/app/main/main-routing.module.ts
@@ -7,6 +7,9 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 // import { ServicesComponent } from './services/services.component';
 import { TeamComponent } from './team/team.component';
 import { PaginationComponent } from './include/pagination/pagination.component';
+
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
+
 // import { WebexpressComponent } from './services/webexpress/webexpress.component';
 
 
@@ -20,12 +23,12 @@ const routes: Routes = [
       { path: 'about-us', component: AboutComponent },
       { path: 'contact-us', component: ContactUsComponent},
       { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
-      // { path: 'services', component: ServicesComponent },
       { path: 'team', component: TeamComponent },
       // { path: 'header', component: HeaderComponent },
       // { path: 'footer', component: FooterComponent },
       { path: 'pagination', component: PaginationComponent },
       // { path: 'webexpress', component: WebexpressComponent },
+      { path: 'privacy-policy', component: PrivacyPolicyComponent },
       { path: 'services', loadChildren: () => import('./services/services.module').then(m => m.ServicesModule) },
       { path: 'news', loadChildren: () => import('./news/news.module').then(m => m.NewsModule) },
     ]
